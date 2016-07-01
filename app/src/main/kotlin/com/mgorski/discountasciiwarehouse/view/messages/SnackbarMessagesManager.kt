bb00eb032069da2e8e04e6abfc14ada17f935880
Michał Górski <mgorski@future-processing.com>
2016-07-01 02:14:47 +0200

    fix snackbar on navigation bar and hide recycler view when list is empty

--- app/src/main/kotlin/com/mgorski/discountasciiwarehouse/view/messages/SnackbarMessagesManager.kt
+++ app/src/main/kotlin/com/mgorski/discountasciiwarehouse/view/messages/SnackbarMessagesManager.kt
@@ -4,10 +4,11 @@ import android.app.Activity
 import android.support.annotation.StringRes
 import android.support.design.widget.Snackbar
 import android.view.View
+import com.mgorski.discountasciiwarehouse.R
 
 class SnackbarMessagesManager(private val activity: Activity) : MessagesManager {
     override fun showMessage(messageId: Int, @StringRes actionTextId: Int?, action: ((View) -> Unit)?) {
-        val snackbar = Snackbar.make(activity.window.decorView, messageId, Snackbar.LENGTH_SHORT)
+        val snackbar = Snackbar.make(activity.findViewById(R.id.toolbar) ?: activity.window.decorView, messageId, Snackbar.LENGTH_SHORT)
         if (actionTextId != null && action != null) {
             snackbar.setAction(actionTextId, action)
         }
