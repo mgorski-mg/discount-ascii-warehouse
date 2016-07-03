f68336b982afef7d8f6498d1bfa1631454d440f5
Michał Górski <mgorski@future-processing.com>
2016-07-03 22:26:22 +0200

    remove joda-money dependency

--- app/src/main/kotlin/com/mgorski/discountasciiwarehouse/model/AsciiItem.kt
+++ app/src/main/kotlin/com/mgorski/discountasciiwarehouse/model/AsciiItem.kt
@@ -1,13 +1,3 @@
 package com.mgorski.discountasciiwarehouse.model
 
-import org.joda.money.CurrencyUnit
-import org.joda.money.Money
-
-class AsciiItem(price: Int, val face: String, val stock: Int, val tags: List<String>) {
-    val priceString: String
-
-    init {
-        val money = Money.of(CurrencyUnit.USD, price / 100.0)
-        priceString = money.currencyUnit.symbol + money.amount
-    }
-}
+class AsciiItem(val price: String, val face: String, val stock: Int, val tags: List<String>)
--- app/src/main/kotlin/com/mgorski/discountasciiwarehouse/model/ModelConverters.kt
+++ app/src/main/kotlin/com/mgorski/discountasciiwarehouse/model/ModelConverters.kt
@@ -2,4 +2,4 @@ package com.mgorski.discountasciiwarehouse.model
 
 import com.mgorski.discountasciiwarehouse.network.model.WebAsciiItem
 
-fun WebAsciiItem.toAsciiItem() = AsciiItem(price, face, stock, tags)
+fun WebAsciiItem.toAsciiItem() = AsciiItem("$%.2f".format(price / 100.0), face, stock, tags)
